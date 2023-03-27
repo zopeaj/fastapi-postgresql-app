0eab0a591f38a86d8420ce707aa948ac19adcf34
Ezekiel <zopeaj@gmail.com>
2023-03-27 04:57:36 +0100

    fastapi postgres app

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 
 
 POSTGRESQL_DATABASE_URL = "postgresql+psycopg2://postgres:admin1234@localhost:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(POSTGRESQL_DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
